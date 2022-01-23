d8872844272fd62f327a1dfa251b84d53646efea
felixvl <digitaleservice@gmail.com>
2022-01-23 11:30:12 +0100

    geen jpa en jdbc alleen web

---
+++ src/test/java/nl/cf/azurepogingeen/AzurepogingeenApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.cf.azurepogingeen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzurepogingeenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
